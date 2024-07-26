74a837198e0bdfa1d9cd6e7af20fdd6cbce505ce
Maxim Urschumzew <u.maxim@live.de>
2024-07-27 00:03:15 +0200

    First implementation done.
    It is now possible to run the agda file via js and
    get the result as string.

--- main.go
+++ main.go
@@ -3,11 +3,34 @@ package go_agda_wrapper
 import (
 	"errors"
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello flake")
+}
+
+func createAndWrite(filename string, content string) (*os.File, error) {
+	// create file
+	file, err := os.Create(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// write content to file
+	_, err = file.WriteString(content)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func fileNameWithoutExtension(fileName string) string {
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
 }
 
 func checkPrerequisites() error {
@@ -32,7 +55,7 @@ func checkPrerequisites() error {
 	return nil
 }
 
-func CompileAndRun(file string, compileDir string) (string, error) {
+func CompileAndRun(sourceRoot string, sourceFile string, compileDir string, topLevelValue string) (string, error) {
 	// we check that all prereqs exist
 	err := checkPrerequisites()
 	if err != nil {
@@ -41,12 +64,31 @@ func CompileAndRun(file string, compileDir string) (string, error) {
 
 	// call agda
 	{
-		output, err := exec.Command("agda", "--js", "--compile-dir", compileDir, file).Output()
+		cmd := exec.Command("agda", "--js", "--compile-dir", compileDir, sourceFile)
+		cmd.Dir = sourceRoot
+		output, err := cmd.Output()
 		if err != nil {
 			return "", errors.New(fmt.Sprintf("Running agda not successful\noutput:\n%s, error: %s", output, err))
 		}
 	}
 
-	// We compile
-	return "", errors.New("Could not load file. Not implemented.")
+	// Compute module name
+	modulename := strings.ReplaceAll(fileNameWithoutExtension(sourceFile), "/", ".")
+
+	// write top level file
+	toplevelFile := filepath.Join(compileDir, "main.js")
+	toplevelContent := fmt.Sprintf("var main = require('%s')\nprocess.stdout.write(main['%s'])", "jAgda."+modulename, topLevelValue)
+	createAndWrite(toplevelFile, toplevelContent)
+
+	// call node
+	cmd := exec.Command("node", "main.js")
+	cmd.Env = append(cmd.Env, "NODE_PATH=.")
+	cmd.Dir = compileDir
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Running node not successful\noutput:\n%s, error: %s", output, err))
+	}
+
+	// done
+	return string(output[:]), nil
 }
--- main_test.go
+++ main_test.go
@@ -2,19 +2,51 @@ package go_agda_wrapper
 
 import (
 	"os"
+	"path/filepath"
+	"strconv"
 	"testing"
 )
 
+const testmodule = `
+module Test where
+
+open import Agda.Builtin.String
+
+output : String
+output = "hello"
+`
+
 func TestAll(t *testing.T) {
-	// create temporary directory
-	dname, err := os.MkdirTemp("", "go_agda_wrapper_compilation_")
+	// create temporary directory (source)
+	sourcedir, err := os.MkdirTemp("", "go_agda_wrapper_source_")
 	if err != nil {
 		t.Errorf("Could not create temp dir: %s", err)
+		return
+	}
+
+	// create temporary directory (target)
+	targetdir, err := os.MkdirTemp("", "go_agda_wrapper_target_")
+	if err != nil {
+		t.Errorf("Could not create temp dir: %s", err)
+		return
+	}
+
+	// create source file
+	_, err = createAndWrite(filepath.Join(sourcedir, "Test.agda"), testmodule)
+	if err != nil {
+		t.Errorf("%s", err)
+		return
 	}
 
 	// run compilation
-	_, err = CompileAndRun("myfile.agda", dname)
+	result, err := CompileAndRun(sourcedir, "Test.agda", targetdir, "output")
 	if err != nil {
-		t.Errorf("Compiling and running failed, error: %s", err)
+		t.Errorf("Compiling and running failed, source: %s, target: %s, error: %s", sourcedir, targetdir, err)
+		return
+	}
+
+	if result != "hello" {
+		t.Errorf("Result of execution was %s", strconv.Quote(result))
+		return
 	}
 }
